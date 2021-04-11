9edc16cc1d5f329e4be6fded589ebf0ec5552318
gui2one <guillaume.rouault.fx@gmail.com>
2021-04-11 18:25:18 +0200

    remoe glad as a submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,23 @@
 #include <assert.h>
 #include <string>
 
+#include <glfw/glfw3.h>
 int main(int argc, char **argv)
 {
+
+    if (!glfwInit())
+    {
+        printf("problem with GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(640, 360, "Starter Project", NULL, NULL);
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glfwPollEvents();
+    }
+    printf("Yeah !!!! \n");
     return 0;
 }
